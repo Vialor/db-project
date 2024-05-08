b4a78914f9240f2ed512b9cc97eb753e0752b546
D Meng <Frank_mdai@163.com>
2024-05-08 14:23:51 -0400

    14:23

--- app_server/static/scripts/thread_detail.js
+++ app_server/static/scripts/thread_detail.js
@@ -2,5 +2,5 @@ const csrftoken = $('meta[name="csrf-token"]').attr("content");
 
 $(".message-btn").click(function (e) {
     var threadId = $(this).attr("threadid");
-    window.location.href = `/thread/${threadId}`;
+    window.location.href = `thread/${threadId}`;
   });
